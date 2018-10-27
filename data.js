5d1e178afd1a98c0e588af6b7035b4976da29a20
Rojesh M. Shikhrakar <roman.shikhrakar@gmail.com>
2018-10-27 18:04:27 +0545

    Museum test

---
+++ data.js
@@ -0,0 +1,163 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-r0010399",
+      "name": "R0010399",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1344,
+      "initialViewParameters": {
+        "yaw": 1.3637140767703517,
+        "pitch": 0.2249831482278548,
+        "fov": 1.358385517880895
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.134995446952776,
+          "pitch": 0.054256747237458214,
+          "rotation": 0,
+          "target": "1-r0010382"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 1.0319066603324867,
+          "pitch": 0.08627741105236097,
+          "title": "Airlift Technology<br>",
+          "text": "A Sample of VR Technology for Heritage and Commercial Use<br>"
+        }
+      ]
+    },
+    {
+      "id": "1-r0010382",
+      "name": "R0010382",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1344,
+      "initialViewParameters": {
+        "yaw": 0.4792777250220741,
+        "pitch": 0,
+        "fov": 1.358385517880895
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.5465195956823461,
+          "pitch": -0.02454075992903526,
+          "rotation": 0,
+          "target": "2-r0010384"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "2-r0010384",
+      "name": "R0010384",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1344,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.8879755172014292,
+          "pitch": 0.08983370341578123,
+          "rotation": 0,
+          "target": "3-r0010385"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "3-r0010385",
+      "name": "R0010385",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1344,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [],
+      "infoHotspots": []
+    }
+  ],
+  "name": "Project Title",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": false,
+    "viewControlButtons": false
+  }
+};
